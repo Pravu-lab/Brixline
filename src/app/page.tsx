ff3ef76458a2d7ffa82a41993dd5c73953f09552
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-04-25 18:28:37 +0530

    Home page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import FAQ from "@/Components/FAQ/Faq";
+import FooterSection from "@/Components/Footer";
 import Header from "@/Components/Header";
 import Hero from "@/Components/Hero";
 import HomeCalculation from "@/Components/HomeCalculation";
 import HomePackagesSection from "@/Components/HomePackagesSection";
+import OurServicesSection from "@/Components/OurServicesSection";
+import QualityCommitmentSection from "@/Components/QualityCommitmentSection";
+import Testimonial from "@/Components/Testimonial/Testimonial";
 import WhyChooseUs from "@/Components/WhyChooseUs";
+import Work from "@/Components/Work";
 
 export default function Home() {
   return (
@@ -12,6 +18,12 @@ export default function Home() {
     <WhyChooseUs/>
     <HomeCalculation/>
     <HomePackagesSection/>
+    <OurServicesSection/>
+    <QualityCommitmentSection/>
+    <Work/>
+    <Testimonial/>
+    <FAQ/>
+    <FooterSection/>
     </>
   );
 }
