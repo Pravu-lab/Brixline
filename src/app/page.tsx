711ccd0207b63345b7cfcc498e68634385130234
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-04-25 17:59:08 +0530

    Home page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/Components/Header";
 import Hero from "@/Components/Hero";
+import WhyChooseUs from "@/Components/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
     <Header/>
     <Hero/>
+    <WhyChooseUs/>
     </>
   );
 }
