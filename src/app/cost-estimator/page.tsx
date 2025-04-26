dbee241d7ea11c131ff7558c3ce06cbca2833f3e
Pravas Chandragiri <pravasinbayarea@gmail.com>
2025-04-26 23:30:50 +0530

    update cost-estimator

--- src/app/cost-estimator/page.tsx
+++ src/app/cost-estimator/page.tsx
@@ -6,7 +6,6 @@ import FAQ from '@/Components/FAQ/Faq'
 
 import FooterSection from '@/Components/Footer'
 import Header from '@/Components/Header'
-import React from 'react'
 
 const page = () => {
   return (
