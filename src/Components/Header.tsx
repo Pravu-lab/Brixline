3b7d679615f8b21cb8360adf905577eecc14f471
APPLE <apple@APPLEs-MacBook-Pro.local>
2025-04-26 15:55:40 +0530

    cost estimator update

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -89,7 +89,7 @@ export default function Header() {
                     </span>
                     <Link href="/about">ABOUT US</Link>
                     <Link href="/">HOW IT WORKS</Link>
-                    <Link href="/">COST ESTIMATOR</Link>
+                    <Link href="/cost-estimator">COST ESTIMATOR</Link>
                     <Link href="/contact-us">CONTACT US</Link>
                     <span className="flex items-center gap-1 text-black">
                         <Zap className="w-4 h-4 text-red-500" />
@@ -131,7 +131,7 @@ export default function Header() {
                     </div>
                     <Link href="/about" className="text-black block">ABOUT US</Link>
                     <Link href="/" className="text-black block">HOW IT WORKS</Link>
-                    <Link href="/" className="text-black block">COST ESTIMATOR</Link>
+                    <Link href="/cost-estimator" className="text-black block">COST ESTIMATOR</Link>
                     <Link href="/contact-us" className="text-black block">CONTACT US</Link>
                     <span className="flex items-center gap-1 text-black">
                         <Zap className="w-4 h-4 text-red-500 block" />
