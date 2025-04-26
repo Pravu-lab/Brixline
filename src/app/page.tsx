8138c8e7f730f9263fa378cdddbd3ee01a752596
Rakesh Adak <rakeshadak099@gmail.com>
2025-04-27 01:09:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FAQ from "@/Components/FAQ/Faq";
 import FooterSection from "@/Components/Footer";
+import Header from "@/Components/Header";
 import Hero from "@/Components/Hero";
 import HomeCalculation from "@/Components/HomeCalculation";
 import HomePackagesSection from "@/Components/HomePackagesSection";
@@ -12,7 +13,7 @@ import Work from "@/Components/Work";
 export default function Home() {
   return (
     <>
-    {/* <Header/> */}
+    <Header/> 
     <Hero/>
     <WhyChooseUs/>
     <HomeCalculation/>
