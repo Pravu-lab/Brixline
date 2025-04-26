6553e25c818be9b77f5dcc5a2af9733e797765e3
Rakesh Adak <rakeshadak099@gmail.com>
2025-04-27 03:44:01 +0530

    Updates

--- src/Components/HomeCalculation.tsx
+++ src/Components/HomeCalculation.tsx
@@ -2,27 +2,25 @@ import React from 'react'
 import { Button, Description, Title } from './Tag'
 import Image from 'next/image';
 
-
 const HomeCalculation = () => {
   return (
-    <section className='bg-[#000] text-center py-16 flex flex-col gap-12 items-center relative'>
-        <Title className='text-white'>
-            Home Construction Cost <br/>Calculator
-        </Title>
-        <Description className='text-white'>
-        Your Perfect Home, Designed & Built for You. <br/>Hassle-free, On-Time, and Within Budget.
-        </Description>
-        <Button className='text-white border-white w-[320px]'>
+    <section className='bg-[#000] text-center pt-10 pb-16 flex flex-col gap-2 items-center relative'>
+      <Title className='text-white'>
+        Home Construction Cost <br />Calculator
+      </Title>
+      <Description className='text-white mb-14 font-bold leading-[140%]'>
+        Your Perfect Home, Designed & Built for You. <br />Hassle-free, On-Time, and Within Budget.
+      </Description>
+      <Button className='text-white border-white w-[320px]'>
         CALCULATE NOW
-        </Button>
-        <div className=" absolute w-full h-[30px] bottom-0">
-        <Image
-          src="/Frame 2147225299.png"
-          alt="house-image-2"
-          fill
-          className="object-cover object-left"
-        />
+      </Button>
+      <div className="absolute w-full overflow-hidden bottom-0">
+        <div className="ticker flex nowrap align-baseline h-full">
+          <img src="/scale.svg" alt="Scale Decoration" className="inline-block h-full" />
+          <img src="/scale.svg" alt="Scale Decoration" className="inline-block h-full" />
         </div>
+      </div>
+
     </section>
   )
 }
