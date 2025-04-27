fb1bd85dedfe0aa580a51ab563eb2eb097069ef7
Pravas Chandragiri <pravasinbayarea@gmail.com>
2025-04-27 17:52:26 +0530

    Cost estimator page fixes

--- src/Components/ConstructionPackage.tsx
+++ src/Components/ConstructionPackage.tsx
@@ -28,7 +28,7 @@ const ConstructionPackage = () => {
   const dataKeys = Object.keys(PackageDetails);
 
   return (
-    <Section className="flex flex-col justify-center bg-[#0000001A] !w-full">
+    <Section className="flex flex-col justify-center bg-[#f7f7f7] !w-full">
       <div className=" m-auto text-center">
         <SubTitle>DISCOVER</SubTitle>
         <Title className="text-black">
@@ -57,9 +57,9 @@ const ConstructionCard: React.FC<ConstructionCardProps> = ({ packageDetails }) =
   const [openIndex, setOpenIndex] = useState<number | null>(0);
 
   return (
-    <div className="mt-6 w-full shadow-[0px_34px_64px_0px_#00000017] py-12 px-4 max-h-[728px] overflow-y-scroll bg-white min-w-[316px] sm:max-w-[316px]">
-      <div className="sticky bg-white text-center top-0">
-        <div className="text-black text-[40px] font-medium mb-4">
+    <div className="mt-6 w-full shadow-[0px_34px_64px_0px_#00000017] pb-12 px-4 max-h-[728px] overflow-y-scroll bg-white min-w-[316px] sm:max-w-[316px]">
+      <div className="sticky bg-white text-center top-0 pt-12">
+        <div className="text-black text-[40px] font-extrabold mb-4">
           {packageDetails.price}
           <br />
           <p className="text-sm text-black">{packageDetails.desc}</p>
@@ -71,7 +71,7 @@ const ConstructionCard: React.FC<ConstructionCardProps> = ({ packageDetails }) =
       {packageDetails.data.map((section, index) => {
         const isOpen = openIndex === index;
         return (
-          <div key={section.title} className="py-4 border-b border-[rgba(0,0,0,0.05)]">
+          <div key={section.title} className={`py-3 ${packageDetails.data.length-1 === index ? "pb-0":"border-b border-[rgba(0,0,0,0.05)]"}`}>
             <div
               className="flex justify-between items-center cursor-pointer"
               onClick={() => setOpenIndex(isOpen ? null : index)}
