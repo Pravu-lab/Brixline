052681235174a69d596e02523d0b0d99cecd2324
Rakesh Adak <rakeshadak099@gmail.com>
2025-04-30 00:41:40 +0530

    Update HeroAboutus.tsx

--- src/Components/HeroAboutus.tsx
+++ src/Components/HeroAboutus.tsx
@@ -9,20 +9,17 @@ const AboutHero = () => {
             <div className="relative z-10 h-full flex w-[90%] sm:w-11/12 mx-auto items-start sm:items-center  flex-col sm:flex-row justify-end sm:justify-between">
 
                 {/* Text Section */}
-                <div className="flex justify-end items-start sm:items-end w-fit h-fit sm:h-full">
-
+                <div className="flex justify-end items-start sm:items-end w-fit h-fit sm:h-full sm:pt-[120px]">
                     {/* Desktop Content */}
-                        <Title className="leading-[100%] text-left !text-[40px] sm:!text-[82px] font-normal">
-                            Building Dreams for<br />
-                            Bengaluru People
-                        </Title>
-
-
+                    <Title className="leading-[100%] text-left !text-[40px] sm:!text-[82px] font-normal">
+                        Building Dreams for<br />
+                        Bengaluru People
+                    </Title>
                 </div>
 
                 {/* GetQuote SVG */}
-                <div className='mt-[20px] sm:mt-0 '>
-                <GetQuote classname="justify-start" />
+                <div className='mt-[20px] sm:mt-0'>
+                    <GetQuote classname="justify-start" />
                 </div>
             </div>
 
