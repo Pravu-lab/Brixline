e56f0a4b3f48669d1330ef9957aa843614f331a3
Pravas Chandragiri <pravasinbayarea@gmail.com>
2025-04-30 00:47:46 +0530

    about us changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,12 +6,14 @@ import OurPartners from '@/Components/OurPartners'
 import Header from '@/Components/Header'
 import React from 'react'
 import AboutUsSlider from '@/Components/AboutusSlider'
+import ScrollingText from '@/Components/AboutusTextAnimation'
 
 const page = () => {
   return (
     <>
     <Header/>
 <AboutHero/>
+<ScrollingText/>
 <AboutUsSlider/>
 <Aboutus/>
 <FounderSection/>
