fa57ac377eaa467e3434b00d3eab2b52a255eb1e
Pravas Chandragiri <pravasinbayarea@gmail.com>
2025-04-30 03:23:24 +0530

    Changes

--- src/Components/FounderSection.tsx
+++ src/Components/FounderSection.tsx
@@ -6,7 +6,7 @@ const FounderSection = () => {
     <Section className="relative h-[100vh] !w-full !py-0 md:!px-[77px]">
       {/* Text Section */}
       {/* flex flex-col justify-end m-auto items-center h-fit sm:h-full absolute max-w-[1028px] z-10 bottom-0 w-full top-1/1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 pb-8 */}
-      <div className="absolute z-10 flex flex-col items-start bottom-1/10 w-full max-w-[530px] left-20">
+      <div className="absolute z-10 flex flex-col items-start bottom-1/10 w-full max-w-[530px] left-5 md:left-20">
         <svg xmlns="http://www.w3.org/2000/svg" width="162" height="85" viewBox="0 0 162 85" fill="none">
           <path opacity="0.2" d="M0 85V39.1739L26.0384 0H65.5397L47.0466 39.1739H71.9013V85H0ZM90.0987 85V39.1739L116.137 0H155.638L137.145 39.1739H162V85H90.0987Z" fill="#F55252" />
         </svg>
--- src/Components/GetQuote.tsx
+++ src/Components/GetQuote.tsx
@@ -41,7 +41,7 @@ interface GetQuoteProps {
     return (
         <div className={`relative w-full flex justify-end ${classname}`}>
             <div className='max-w-[390px] relative max-h-[532px]'>
-                <div className=' absolute px-7 pt-[72px] z-20'>
+                <div className=' absolute px-7 pt-[50px] md:pt-[72px] z-20'>
                     <span className='absolute bg-[#F55252] text-xs px-5 py-[7px] font-bold top-0 left-1/4 rounded-bl-2xl rounded-br-2xl'>
                         FREE CONSULTATION
                     </span>
